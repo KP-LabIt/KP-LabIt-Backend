013523591604aa7afb928eb77f1c118e1f0450ff
dankouuu <r7dhbmwsyc@privaterelay.appleid.com>
2025-12-01 13:36:47 +0100

    pridanie password url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import get_init, login
+from .views import get_init, login, change_password
 
 urlpatterns = [
     path("", get_init, name="get_init"),
     path("login/", login, name="login"),
+    path("change_password/", change_password, name="change_password"),
+
 ]
